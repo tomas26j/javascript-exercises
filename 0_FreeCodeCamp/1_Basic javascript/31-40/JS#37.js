aed1a6710984fd9d492ac8c53fcfca5666a70186
Tomas Riera <53879865+tomas26j@users.noreply.github.com>
2023-09-19 19:37:18 -0300

    freecodecamp JS 36-40

---
+++ 0_FreeCodeCamp/1_Basic javascript/31-40/JS#37.js
@@ -0,0 +1,12 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+
+const wordBlanks = myNoun + ' ' + myAdjective + ' ' + myVerb + ' ' + myAdverb;
+
+const wordBlanks2 = `${myNoun} ${myAdjective} ${myVerb} ${myAdverb}`;//Better solution
+
+// Only change code above this line
---
+++ 0_FreeCodeCamp/1_Basic javascript/31-40/JS#39.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+const myArray = [
+  ["first child", 1],
+  ["second child", 2],
+];
