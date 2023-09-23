b8037b5e0edaa24bf50b25e33903e92768f35820
Tomas Riera <53879865+tomas26j@users.noreply.github.com>
2023-09-22 23:32:36 -0300

    Create JS#53.js

---
+++ 0_FreeCodeCamp/1_Basic javascript/51-60/JS#53.js
@@ -0,0 +1,14 @@
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  let outerWear = 'sweater';
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+console.log(outerWear); //'T-shirt'
+console.log(myOutfit());//'sweater'
