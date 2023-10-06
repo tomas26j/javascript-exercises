97b14fbaf23b29935c27d74bce4af5f4344bbbd7
Tomas Riera <53879865+tomas26j@users.noreply.github.com>
2023-10-06 03:23:50 -0300

    Create JS#86.js

---
+++ 0_FreeCodeCamp/1_Basic javascript/81-90/JS#86.js
@@ -0,0 +1,10 @@
+const myDog = {
+  name: "Coder",
+  legs: 4,
+  tails: 1,
+  friends: ["freeCodeCamp Campers"],
+};
+
+myDog.name = "Happy Coder";
+//myDog['name'] = 'Happy Coder'; //solution 2
+//myDog[0] = 'Happy Coder'; //solution 3
